283581e774c147a5b79b3ca986f9977b3c9e653d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 09:27:08 -0400

    Update nothing.py

--- src/vba_types/nothing.py
+++ src/vba_types/nothing.py
@@ -1,4 +1,8 @@
-from typing import Any, Optional
+from typing import Any, Optional, TypeVar
+
+
+T = TypeVar('T', bound='VBANothing')
+
 
 class VBANothing:
     """
@@ -12,27 +16,27 @@ class VBANothing:
             cls._instance = super(VBANothing, cls).__new__(cls)
         return cls._instance
 
-    def __repr__(self) -> str:
+    def __repr__(self: T) -> str:
         return "Nothing"
 
-    def __str__(self) -> str:
+    def __str__(self: T) -> str:
         # In VBA, printing Nothing usually results in an error, 
         # but for Python interoperability, we return its repr or empty.
         return "Nothing"
 
-    def __bool__(self) -> bool:
+    def __bool__(self: T) -> bool:
         return False
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self: T, other: Any) -> bool:
         # 'Nothing' only equals itself. 
         # In VBA, you'd usually use 'If obj Is Nothing'
         return isinstance(other, VBANothing)
 
     # Disable math/string operations to mimic VBA's "Object not set" errors
-    def __add__(self, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
-    def __sub__(self, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
-    def __mul__(self, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
-    def __truediv__(self, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
+    def __add__(self: T, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
+    def __sub__(self: T, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
+    def __mul__(self: T, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
+    def __truediv__(self: T, other): raise TypeError("VBA Error 91: Object variable or With block variable not set")
 
 
 Nothing = VBANothing()
