23ff25dfbdbdd21af559bf0f8c93c376343f1679
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 09:03:24 -0400

    Update empty.py

--- src/vba_types/empty.py
+++ src/vba_types/empty.py
@@ -1,6 +1,11 @@
-from typing import Any, Optional, Union
+from typing import Any, Optional, TypeVar, Union
+from .vba_type_base import VBATypeBase
 
-class VBAEmpty:
+
+T = TypeVar("T", bound="VBAEmpty")
+
+
+class VBAEmpty(VBATypeBase):
     """
     Represents the VBA 'Empty' type. 
     It is initialized to 0 in a numeric context and "" in a string context.
@@ -12,23 +17,23 @@ class VBAEmpty:
             cls._instance = super(VBAEmpty, cls).__new__(cls)
         return cls._instance
 
-    def __repr__(self) -> str:
+    def __repr__(self: T) -> str:
         return "Empty"
 
-    def __str__(self) -> str:
+    def __str__(self: T) -> str:
         return ""
 
-    def __int__(self) -> int:
+    def __int__(self: T) -> int:
         return 0
 
-    def __float__(self) -> float:
+    def __float__(self: T) -> float:
         return 0.0
 
-    def __bool__(self) -> bool:
+    def __bool__(self: T) -> bool:
         # In VBA, Empty evaluates to False/0
         return False
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self: T, other: Any) -> bool:
         if isinstance(other, VBAEmpty):
             return True
         if isinstance(other, (int, float)):
@@ -38,13 +43,13 @@ class VBAEmpty:
         return False
 
     # Arithmetic behavior (Empty acts as 0)
-    def __add__(self, other: Any) -> Any: return 0 + other
-    def __radd__(self, other: Any) -> Any: return other + 0
-    def __sub__(self, other: Any) -> Any: return 0 - other
-    def __rsub__(self, other: Any) -> Any: return other - 0
-    def __mul__(self, other: Any) -> Any: return 0 * other
-    def __rmul__(self, other: Any) -> Any: return other * 0
-    def __truediv__(self, other: Any) -> Any: return 0 / other
+    def __add__(self: T, other: Any) -> Any: return 0 + other
+    def __radd__(self: T, other: Any) -> Any: return other + 0
+    def __sub__(self: T, other: Any) -> Any: return 0 - other
+    def __rsub__(self: T, other: Any) -> Any: return other - 0
+    def __mul__(self: T, other: Any) -> Any: return 0 * other
+    def __rmul__(self: T, other: Any) -> Any: return other * 0
+    def __truediv__(self: T, other: Any) -> Any: return 0 / other
 
 
 Empty = VBAEmpty()
