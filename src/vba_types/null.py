d72645cef8bba214bc6670bb82953f93a2d904a6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 11:17:32 -0400

    Update null.py

--- src/vba_types/null.py
+++ src/vba_types/null.py
@@ -11,7 +11,7 @@ class VBANull:
     """
     _instance: Optional['VBANull'] = None
 
-    def __new__(cls) -> 'VBANull':
+    def __new__(cls:Type[T]) -> 'VBANull':
         if cls._instance is None:
             cls._instance = super(VBANull, cls).__new__(cls)
         return cls._instance
@@ -23,7 +23,7 @@ class VBANull:
         return "Null"
 
     def __bool__(self: T) -> bool:
-        # In VBA, 'If Null Then' results in an error or False-like behavior 
+        # In VBA, 'If Null Then' results in an error or False-like behavior
         # depending on context, but it is effectively falsy in Python.
         return False
 
@@ -41,9 +41,12 @@ class VBANull:
     # Note: In Python, __eq__ must return a boolean for dict/set keys to work.
     # To mimic VBA's "If x = Null" always being False, we return False for equality.
     def __eq__(self: T, other: Any) -> bool:
-        return False  # In VBA, Null = Null is False (use IsNull instead)
+        # In VBA, Null = Null is False (use IsNull instead)
+        return False  
 
     def __ne__(self: T, other: Any) -> bool:
-        return True # In VBA, Null <> anything is also effectively handled as not-equal
+        # In VBA, Null <> anything is also effectively handled as not-equal
+        return True
+
 
 Null = VBANull()
