81193f5c25d0bb1b1c3a20fa5b2128931224bb01
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 08:31:44 -0400

    Define public API in __init__.py
    Add __all__ to define public API for the module

--- src/vba_types/__init__.py
+++ src/vba_types/__init__.py
@@ -1,3 +1,9 @@
 from .array import VBAArray
 from .integer import VBAInteger
 from .literal_factory import literal_from_string
+
+__all__ = [
+    "VBAArray",
+    "VBAInteger",
+    "lieral_from_string",
+]
