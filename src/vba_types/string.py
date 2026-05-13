d4589d686722779175115d32ceefd36175f986ca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 13:26:45 -0400

    Update string.py

--- src/vba_types/string.py
+++ src/vba_types/string.py
@@ -6,7 +6,7 @@ T = TypeVar('T', bound='VBAString')
 
 
 class VBAString(VBATypeBase):
-    def __init__(self: t, value: str = "") -> None:
+    def __init__(self: T, value: str = "") -> None:
         self.value = value
 
     def __str__(self: T) -> str:
