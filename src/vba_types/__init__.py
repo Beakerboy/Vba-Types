7943208eaa5708f368eb4724795de1d5f6eacfb3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 14:14:35 -0400

    Update __init__.py

--- src/vba_types/__init__.py
+++ src/vba_types/__init__.py
@@ -1,4 +1,5 @@
 from .array import VBAArray
+from .boolean import VBABoolean
 from .double import VBADouble
 from .empty import VBAEmpty
 from .integer import VBAInteger
@@ -6,6 +7,9 @@ from .literal_factory import literal_from_string
 
 __all__ = [
     "VBAArray",
+    "VBABoolean",
+    "VBADouble",
+    "VBAEmpty",
     "VBAInteger",
     "literal_from_string",
 ]
