fbf49342e5bd125a0e74514ecaa9ee33f5fb828b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 11:57:11 -0400

    Update integer.py

--- src/vba_types/integer.py
+++ src/vba_types/integer.py
@@ -84,7 +84,7 @@ class VBAInteger(VBATypeBase):
         # VBA '/' always returns a Double (float in Python)
         if other == 0:
             raise DivisionByZeroError()
-        return VBADouble(float(self.value) / float(other.value))
+        return vba_types.double.VBADouble(float(self.value) / float(other.value))
 
     def __floordiv__(self: T, other: VBACompatible) -> T:
         # VBA '\' is integer division
