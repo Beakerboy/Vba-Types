e81035c3b9e5672387a3851589904b33c5ee91e3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 15:45:50 -0400

    Update array.py

--- src/vba_types/array.py
+++ src/vba_types/array.py
@@ -67,7 +67,7 @@ class VBAArray(VBATypeBase):
 
     def _recursive_init(self: T, shape: Tuple[int, ...]) -> Any:
         if len(shape) == 1:
-            return [Empty] * shape[0]
+            return [vba_types.empty.Empty] * shape[0]
         return [self._recursive_init(shape[1:]) for _ in range(shape[0])]
 
     def _get_coords(self: T, indices: Tuple[int, ...]) -> Tuple[int, ...]:
