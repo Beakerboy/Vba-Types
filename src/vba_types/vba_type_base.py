7e09159e5add7e2910aa740d9962c8b3859651d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 08:06:30 -0400

    Update vba_type_base.py

--- src/vba_types/vba_type_base.py
+++ src/vba_types/vba_type_base.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 from abc import ABC
-from typing import Any
+from typing import Any, TypeVar
 
+
+T = TypeVar('T', bound='VBATypeBase')
 
 class VBATypeBase(ABC):
     value: Any
+    def __add__(self: T, other: T) -> T:
+        from vba_registry import registry
+        return registry.execute_binary_op("+", self, other)
