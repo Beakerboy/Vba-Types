36850d02937aab2e8012df4364acece220425ba6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 21:17:44 -0400

    Update boolean.py

--- src/vba_types/boolean.py
+++ src/vba_types/boolean.py
@@ -40,5 +40,5 @@ class VBABoolean(VBATypeBase):
             raise TypeMismatchError()
         return VBABoolean(self.value < other.value)
 
-    def vba_and(self: T, other: VBABoolean) -> VBABoolean:
+    def vba_and(self: T, other: T) -> T
         return VBABoolean(bool(self) and bool(other))
