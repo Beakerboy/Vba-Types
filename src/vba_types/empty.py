860bf95248c1feca190889335ed57bb546122668
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 06:58:55 -0400

    Refactor arithmetic methods in empty.py
    Refactor arithmetic methods for better readability.

--- src/vba_types/empty.py
+++ src/vba_types/empty.py
@@ -55,10 +55,17 @@ class VBAEmpty(VBATypeBase):
         return vba_types.boolean.VBABoolean(0 < other.value)
 
     # Arithmetic behavior (Empty acts as 0)
-    def __add__(self: T, other: Any) -> Any: return type(other)(0 + other.value)
-    def __sub__(self: T, other: Any) -> Any: return type(other)(0 - other.value)
-    def __mul__(self: T, other: Any) -> Any: return type(other)(0 * other.value)
-    def __truediv__(self: T, other: Any) -> Any: return type(other)(0 / other.value)
+    def __add__(self: T, other: Any) -> Any: 
+        return type(other)(0 + other.value)
+        
+    def __sub__(self: T, other: Any) -> Any: 
+        return type(other)(0 - other.value)
+        
+    def __mul__(self: T, other: Any) -> Any:
+        return type(other)(0 * other.value)
+        
+    def __truediv__(self: T, other: Any) -> Any:
+        return type(other)(0 / other.value)
 
 
 Empty = VBAEmpty()
