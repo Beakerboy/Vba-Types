a6c4802d1695eb8cf84e6885da81a59e6d70785b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 07:34:44 -0400

    Optimize loop with range in recursive_init method

--- src/vba_types/array.py
+++ src/vba_types/array.py
@@ -68,7 +68,8 @@ class VBAArray(VBATypeBase):
     def _recursive_init(self: T, shape: Tuple[int, ...], empty: VBATypeBase) -> Any:
         if len(shape) == 1:
             return [empty] * shape[0]
-        return [self._recursive_init(shape[1:], empty) for _ in range(shape[0])]
+        rng = range(shape[0])
+        return [self._recursive_init(shape[1:], empty) for _ in rng]
 
     def _get_coords(self: T, indices: Tuple[int, ...]) -> Tuple[int, ...]:
         if len(indices) != len(self._bounds):
