c22e8527bb015177ff08586045c6016dbb5f954f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 08:21:58 -0400

    Update vba_registry.py

--- src/vba_types/vba_registry.py
+++ src/vba_types/vba_registry.py
@@ -16,16 +16,23 @@ BinaryOpMap = Dict[Tuple[str, str, str], Callable[[Any, Any], 'VBAValue']]
 
 
 class VBARegistry:
-    def __init__(self: T):
+    def __init__(self: T) -> None:
         self._binary_ops: BinaryOpMap = {}
 
-    def register_binary(self: T, op: str, type_left: str, type_right: str, handler: Callable):
+    def register_binary(self: T,
+                        op: str,
+                        type_left: str,
+                        type_right: str,
+                        handler: Callable) -> None:
         self._binary_ops[(op, type_left, type_right)] = handler
 
-    def execute_binary_op(self: T, op: str, left: VBAValue, right: VBAValue) -> VBAValue:
+    def execute_binary_op(self: T,
+                          op: str,
+                          left: VBAValue,
+                          right: VBAValue) -> VBAValue:
         key = (op, left.type_name, right.type_name)
         handler = self._binary_ops.get(key)
-        
+
         if not handler:
             raise TypeError(self._get_vba_error_msg(op, left, right))
         return handler(left, right)
