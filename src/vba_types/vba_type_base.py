53a95326139eb6f6d694839aa13ecbbf308c4d4d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 07:50:38 -0400

    Update vba_type_base.py

--- src/vba_types/vba_type_base.py
+++ src/vba_types/vba_type_base.py
@@ -1,2 +1,8 @@
-class VBATypeBase:
-    pass
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class VBATypeBase(ABC):
+    def __init__(self, value: Any):
+        self.value = value
