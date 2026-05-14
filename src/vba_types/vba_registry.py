afdd0d33e0a62455796e23544d61177537403f50
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 08:16:36 -0400

    Update vba_registry.py

--- src/vba_types/vba_registry.py
+++ src/vba_types/vba_registry.py
@@ -6,26 +6,34 @@ if TYPE_CHECKING:
     from vba_base import VBAValue
 
 
+T = TypeVar('T', bound='VBARegistry')
+
+
 # Maps: (operator, left_type, right_type) -> calculation function
 BinaryOpMap = Dict[Tuple[str, str, str], Callable[[Any, Any], 'VBAValue']]
 
 
 class VBARegistry:
-    def __init__(self):
+    def __init__(self: T):
         self._binary_ops: BinaryOpMap = {}
 
-    def register_binary(self, op: str, type_left: str, type_right: str, handler: Callable):
+    def register_binary(self: T, op: str, type_left: str, type_right: str, handler: Callable):
         self._binary_ops[(op, type_left, type_right)] = handler
 
-    def execute_binary_op(self, op: str, left: VBAValue, right: VBAValue) -> VBAValue:
+    def execute_binary_op(self: T, op: str, left: VBAValue, right: VBAValue) -> VBAValue:
         key = (op, left.type_name, right.type_name)
         handler = self._binary_ops.get(key)
         
         if not handler:
-            raise TypeError(self._get_vba_error_msg(op, left, right))
+            raise TypeError(self: T._get_vba_error_msg(op, left, right))
         return handler(left, right)
 
-    def _get_vba_error_msg(self, op: str, left: VBAValue, right: VBAValue) -> str:
-        return f"Run-time error '13': Type mismatch for {left.type_name} {op} {right.type_name}"
+    def _get_vba_error_msg(self: T,
+                           op: str,
+                           left: VBAValue,
+                           right: VBAValue) -> str:
+        return (f"Run-time error '13': Type mismatch for "
+                f"{left.type_name} {op} {right.type_name}"
+
 
 registry = VBARegistry()
