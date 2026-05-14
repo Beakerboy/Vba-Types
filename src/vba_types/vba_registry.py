0b764c3784280d9a5664a1ec5fa83b8d309f745f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 08:19:06 -0400

    Add TypeVar import to vba_registry.py

--- src/vba_types/vba_registry.py
+++ src/vba_types/vba_registry.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
-from typing import Any, Callable, Dict, Tuple, TYPE_CHECKING
+from typing import (
+    Any, Callable, Dict, Tuple, TypeVar, TYPE_CHECKING
+)
 
 
 if TYPE_CHECKING:
