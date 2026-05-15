69935457dec335b5e01d495adf58b5dbe7334fc5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 11:03:40 -0400

    Update vba_type_base.py

--- src/vba_types/vba_type_base.py
+++ src/vba_types/vba_type_base.py
@@ -4,32 +4,11 @@ from typing import Any, TypeVar
 from enum import Enum, auto
 
 
-class VBAType(Enum):
-    ARRAY = auto()
-    BOOLEAN = auto()
-    BYTE = auto()
-    CURRENCY = auto()
-    DATE = auto()
-    DOUBLE = auto()
-    EMPTY = auto()
-    INTEGER = auto()
-    LONG = auto()
-    LONGLONG = auto()
-    NULL = auto()
-    STRING = auto()
-
-
 T = TypeVar('T', bound='VBATypeBase')
 
 
 class VBATypeBase(ABC):
     value: Any
-
-    # In your base class:
-    @property
-    @abstractmethod
-    def type_name(self: T) -> VBAType:
-        pass
 
     def __add__(self: T, other: T) -> T:
         from vba_types.vba_registry import registry
