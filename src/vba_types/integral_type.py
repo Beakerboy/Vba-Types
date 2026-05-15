f0c5ae6a3b3a15073063555c2c40d94fe61ffae4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 15:17:54 -0400

    Update integral_type.py

--- src/vba_types/integral_type.py
+++ src/vba_types/integral_type.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 from .exceptions import OverflowException
 from .vba_type_base import VBATypeBase
-from typing import Any, TypeVar
+from typing import Any, TypeVar, TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from .boolean import VBABoolean
 
 
 T = TypeVar("T", bound="VBAIntegralType")
