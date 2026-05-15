42d30dcc9625f03bd427197122387208e33752d0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 14:03:24 -0400

    Update integral_type.py

--- src/vba_types/integral_type.py
+++ src/vba_types/integral_type.py
@@ -13,16 +13,7 @@ class VBAIntegralType(VBATypeBase):
     def __init__(self: T, value: int = 0) -> None:
         self.value = self._validate(value)
 
-    def _validate(self: T, value: VBATypeBase) -> int:
-        # Extract raw numeric value
-        if isinstance(value, VBATypeBase):
-            raw_val = value.value
-        else:
-            raw_val = int(value)
-        # VBA uses 'Banker's Rounding'
-        # (rounds to nearest even number)
-        final_val: int = int(round(raw_val))
-
+    def _validate(self: T, value: int) -> int:
         if not (self.MIN_VALUE <= final_val <= self.MAX_VALUE):
             raise OverflowError("Run-time error '6': Overflow")
         return final_val
