ad90e1b6b7d8f2063b0895eb1a393b0b28f09216
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 22:46:12 -0400

    Update array.py

--- src/vba_types/array.py
+++ src/vba_types/array.py
@@ -1,4 +1,4 @@
-rom __future__ import annotations
+from __future__ import annotations
 from functools import total_ordering
 from typing import Any, Tuple, Type, TypeVar, Union
 from .exceptions import SubscriptOutOfRangeError
