bddb9bbaebadfe3b58b8107156dbff1bdb8e6d31
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 09:26:57 -0400

    Create vba_operations.py

---
+++ src/vba_types/vba_operations.py
@@ -0,0 +1,10 @@
+from .vba_registry import registry:
+from vba_types.null import Null, VBANull
+
+
+def handle_null_propogation(left, right):
+    return Null
+
+
+registry.register("+", VBANull, VBATypeBase, handle_null_propogation)
+registry.register("+", VBATypeBase, VBANull, handle_null_propogation)
