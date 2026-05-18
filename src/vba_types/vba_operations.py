e5212b80f26e3d4e6badd34c81102d4e4fa34f7c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 12:30:40 -0400

    Update vba_operations.py

--- src/vba_types/vba_operations.py
+++ src/vba_types/vba_operations.py
@@ -74,22 +74,22 @@ def _numeric_inequality(left: VBATypeBase,
 
 def _numeric_lt(left: VBATypeBase,
                 right: VBATypeBase) -> VBABoolean:
-    return VBABoolean(left.value < right.val
+    return VBABoolean(left.value < right.val)
 
 
 def _numeric_gt(left: VBATypeBase,
                 right: VBATypeBase) -> VBABoolean:
-    return VBABoolean(left.value > right.val
+    return VBABoolean(left.value > right.val)
 
 
 def _numeric_ge(left: VBATypeBase,
                 right: VBATypeBase) -> VBABoolean:
-    return VBABoolean(left.value >= right.val
+    return VBABoolean(left.value >= right.val)
 
 
 def _numeric_le(left: VBATypeBase,
                 right: VBATypeBase) -> VBABoolean:
-    return VBABoolean(left.value <= right.val
+    return VBABoolean(left.value <= right.val)
 
 
 registry.register("+", VBANull, VBATypeBase, handle_null_propogation)
