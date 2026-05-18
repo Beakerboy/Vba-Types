8c5818db65b4bbfcd8d889c4776ea7bf84d3f1b0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 10:35:40 -0400

    Update vba_operations.py

--- src/vba_types/vba_operations.py
+++ src/vba_types/vba_operations.py
@@ -2,18 +2,26 @@ from .vba_registry import registry
 from vba_types.null import Null, VBANull
 from vba_types.vba_type_base import VBATypeBase
 from vba_types.boolean import VBABoolean
+from vba_types.double import VBADouble
 from vba_types.empty import VBAEmpty
+from vba_types.integral_type import VBAIntegralType
 
 
 
 def handle_null_propogation(left: VBATypeBase, right: VBATypeBase) -> VBANull:
     return Null
 
-def add_promote_to_integer(left: VBATypeBase, right: VBATypeBase) -> VBAInteger:
+def _add_promote_to_integer(left: VBATypeBase, right: VBATypeBase) -> VBAInteger:
     return VBAInteger(left.value + right.value)
 
-def _subpromote_to_integer(left: VBATypeBase, right: VBATypeBase) -> VBAInteger:
+def _sub_promote_to_integer(left: VBATypeBase, right: VBATypeBase) -> VBAInteger:
     return VBAInteger(left.value + right.value)
+
+def _subpromote_to_double(left: VBATypeBase, right: VBATypeBase) -> VBADouble:
+    return VBADouble(left.value + right.value)
+
+def _sub_promote_to_double(left: VBATypeBase, right: VBATypeBase) -> VBADouble:
+    return VBADouble(left.value - right.value)
 
 registry.register("+", VBANull, VBATypeBase, handle_null_propogation)
 registry.register("+", VBATypeBase, VBANull, handle_null_propogation)
@@ -24,11 +32,16 @@ registry.register("*", VBATypeBase, VBANull, handle_null_propogation)
 registry.register("/", VBANull, VBATypeBase, handle_null_propogation)
 registry.register("/", VBATypeBase, VBANull, handle_null_propogation)
 
-registry.register("+", VBAInteger, VBAInteger, add_promote_to_integer)
-registry.register("+", VBABoolean, VBAInteger, add_promote_to_integer)
-registry.register("+", VBAInteger, VBABoolean, add_promote_to_integer)
-registry.register("+", VBAEmpty, VBAEmpty, add_promote_to_integer)
-registry.register("-", VBAInteger, VBAInteger, sub_promote_to_integer)
-registry.register("-", VBABoolean, VBAInteger, sub_promote_to_integer)
-registry.register("-", VBAInteger, VBABoolean, sub_promote_to_integer)
-registry.register("-", VBAEmpty, VBAEmpty, sub_promote_to_integer)
+registry.register("+", VBAInteger, VBAInteger, _add_promote_to_integer)
+registry.register("+", VBABoolean, VBAInteger, _add_promote_to_integer)
+registry.register("+", VBAInteger, VBABoolean, _add_promote_to_integer)
+registry.register("+", VBAEmpty, VBAEmpty, _add_promote_to_integer)
+registry.register("-", VBAInteger, VBAInteger, _sub_promote_to_integer)
+registry.register("-", VBABoolean, VBAInteger, _sub_promote_to_integer)
+registry.register("-", VBAInteger, VBABoolean, _sub_promote_to_integer)
+registry.register("-", VBAEmpty, VBAEmpty, _sub_promote_to_integer)
+
+registry.register("+", VBAIntegralType, VBADouble, _add_promote_to_double)
+registry.register("+", VBADouble, VBAIntegralType, _add_promote_to_double)
+registry.register("-", VBAIntegralType, VBADouble, _sub_promote_to_double)
+registry.register("-", VBADouble, VBAIntegralType, _sub_promote_to_double)
