a2fd32070deefc5232e715b98fe0f3729a14a1a9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 14:00:58 -0400

    Update vba_operations.py

--- src/vba_types/vba_operations.py
+++ src/vba_types/vba_operations.py
@@ -14,8 +14,10 @@ from .exceptions import DivisionByZeroError, TypeMismatchError
 def handle_null_propogation(left: VBATypeBase, right: VBATypeBase) -> VbaNull:
     return Null
 
+
 def type_mismatch(left: VBATypeBase, right: VBATypeBase) -> None:
     raise TypeMismatchError
+
 
 def _add_promote_to_integer(left: VBATypeBase,
                             right: VBATypeBase) -> VBAInteger:
