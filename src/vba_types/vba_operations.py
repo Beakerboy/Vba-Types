7488addb5787db1c3cb98f39b9c5c38febb03bac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 09:53:35 -0400

    Update vba_operations.py

--- src/vba_types/vba_operations.py
+++ src/vba_types/vba_operations.py
@@ -2,7 +2,7 @@ from .vba_registry import registry
 from vba_types.null import Null, VBANull
 
 
-def handle_null_propogation(left, right):
+def handle_null_propogation(left: VBATypeBase, right: VBATypeBase):
     return Null
 
 
