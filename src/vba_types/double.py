d9a2514199060add4cf79f1714fa82f26e2b1cee
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 13:21:04 -0400

    Update double.py

--- src/vba_types/double.py
+++ src/vba_types/double.py
@@ -20,7 +20,7 @@ class VBADouble(VBAFloatType):
 
     value: float
 
-    def __init__(self: T, value: 0.0) -> None:
+    def __init__(self: T, value: float = 0.0) -> None:
         # Avoid double validation if we are already dealing with a verified
         # float
         if isinstance(value, float):
