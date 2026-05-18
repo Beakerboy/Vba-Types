f0f1c4c143be94c9c799b39ff92ffdda2ffb0fa4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 12:08:53 -0400

    Update float_type.py

--- src/vba_types/float_type.py
+++ src/vba_types/float_type.py
@@ -1,11 +1,5 @@
 from __future__ import annotations
-from .exceptions import OverflowException
 from .numeric_type import VBANumericType
-from typing import Any, TypeVar, TYPE_CHECKING
-
-
-if TYPE_CHECKING:
-    from .boolean import VBABoolean
 
 
 T = TypeVar("T", bound="VBAFloatType")
