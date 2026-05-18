6ca864cfecfe34db631fb8dfeb2eefed8558f017
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 12:51:47 -0400

    Update string.py

--- src/vba_types/string.py
+++ src/vba_types/string.py
@@ -11,6 +11,3 @@ class VBAString(VBATypeBase):
 
     def __str__(self: T) -> str:
         return self.value
-
-    def __add__(self: T, other: VBATypeBase) -> T:
-        return type(self)(self.value + str(other.value))
