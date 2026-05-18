4df2c942b798784978437705359f4c0331d6f6f5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 13:06:01 -0400

    Update test_integer.py

--- tests/Unit/test_integer.py
+++ tests/Unit/test_integer.py
@@ -52,7 +52,7 @@ def test_basic_math_operations() -> None:
 
     # Power
     val = b ** VBAInteger(2)
-    assert val.value == 9
+    assert val.value == 9.0
     assert isinstance(val, VBADouble)
 
 
