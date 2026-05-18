ee2c3b57521d63b425dd2fade13608ca44a8e58b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 14:20:24 -0400

    Update vba_operations.py

--- src/vba_types/vba_operations.py
+++ src/vba_types/vba_operations.py
@@ -34,6 +34,16 @@ def _mul_promote_to_integer(left: VBATypeBase,
     return VBAInteger(left.value * right.value)
 
 
+def _mod_promote_to_integer(left: VBATypeBase,
+                            right: VBATypeBase) -> VBAInteger:
+    return VBAInteger(left.value % right.value)
+
+
+def _mod_promote_to_integer(left: VBATypeBase,
+                            right: VBATypeBase) -> VBAInteger:
+    return VBALong(left.value % right.value)
+
+
 def _add_promote_to_double(left: VBATypeBase, right: VBATypeBase) -> VBADouble:
     return VBADouble(left.value + right.value)
 
@@ -192,6 +202,8 @@ registry.register("*", VBAInteger, VBABoolean, _mul_promote_to_integer)
 registry.register("*", VBAInteger, VBAEmpty, _mul_promote_to_integer)
 registry.register("*", VBAEmpty, VBAInteger, _mul_promote_to_integer)
 registry.register("*", VBAEmpty, VBAEmpty, _mul_promote_to_integer)
+registry.register("%", VBAInteger, VBADouble, _mod_promote_to_integer)
+registry.register("%", VBAInteger, VBAInteger, _mod_promote_to_long)
 
 registry.register("*", VBADouble, VBADouble, _mul_promote_to_double)
 registry.register("*", VBAIntegralType, VBADouble, _mul_promote_to_double)
