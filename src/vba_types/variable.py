cae807f5e09d70b429e2d1da98574a59a0d65520
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 14:29:01 -0400

    Update variable.py

--- src/vba_types/variable.py
+++ src/vba_types/variable.py
@@ -7,8 +7,8 @@ T = TypeVar('T', bound='VBAVariable')
 
 
 class VBAVariable:
-    def __init__(self: T, declared_type: str, value: Optional[VBAVariable | VBATypeBase] = None) -> None:
-        self.declared_type = declared_type
+    def __init__(self: T, declared_type: str = "Variant", value: Optional[VBAVariable | VBATypeBase] = None) -> None:
+        self._declared_type = declared_type
         if value is None:
             value = Empty
         self.value = value
@@ -23,11 +23,15 @@ class VBAVariable:
         return return self._value + self._unwrap(other)
 
     @property
-    def value(self: T):
+    def declared_type(self: T) -> str:
+        return self._declared_type
+
+    @property
+    def value(self: T) -> VBATypeBase:
         return self._value
 
     @value.setter
-    def value(self: T, incoming: VBAVariable | VBATypeBase):
+    def value(self: T, incoming: VBAVariable | VBATypeBase) -> None:
         # Unwrap incoming value if it is another variable container
         if isinstance(incoming, VBAVariable):
             incoming = incoming.value
