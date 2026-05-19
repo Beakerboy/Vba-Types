84460bf092565236f088f10b3752079329178d96
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 16:12:14 -0400

    Update types_registry.py

--- src/vba_types/types_registry.py
+++ src/vba_types/types_registry.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import TypeVar
-from .vba_type_base import VBATypeBase
+from .vba_type_base import Callable, VBATypeBase
 
 
 T = TypeVar('T', bound='VBATypesRegistry')
