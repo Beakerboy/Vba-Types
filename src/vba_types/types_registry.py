6aeffcc69303b5018e7a528ac6b9918ecdc050db
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 16:11:07 -0400

    Update types_registry.py

--- src/vba_types/types_registry.py
+++ src/vba_types/types_registry.py
@@ -12,15 +12,15 @@ class VBATypesRegistry:
         self._registry: dict[tuple[str, type], Callable] = {}
 
     def register(self: T,
-                 op: str,
+                 type_name: str,
                  incoming_cls: type,
                  handler: Callable) -> None:
-        self._registry[(op, incoming_cls)] = handler
+        self._registry[(type_name, incoming_cls)] = handler
 
     def coerce(self: T,
-                type: str,
+                type_name: str,
                 incoming: VBATypeBase) -> VBATypeBase:
-        handler = self._get_handler(type, type(incoming)))
+        handler = self._get_handler(type_name, type(incoming))
 
         if not handler:
             raise TypeError(self._get_vba_error_msg(op, left, right))
