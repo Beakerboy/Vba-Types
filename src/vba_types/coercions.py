585180889606f7d066552b1da77cd1822b511b67
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 15:51:09 -0400

    Create coercions.py

---
+++ src/vba_types/coercions.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from .exception import TypeMismatchError, InvalidNullError
+from .types_registry import registry
+from .null import VBANull
+
+
+def type_error(value: VBATypeBase) -> None:
+    raise TypeMismatchError()
+
+
+def invalid_null(value: VBATypeBase) -> None:
+    raise InvalidNullError()
+
+
+def return_self(value: VBATypeBase) -> VBATypeBase:
+    return value
+
+registry.register("*", VBANull, invalid_null)
+registry.register("*", VBAArray, type_error)
