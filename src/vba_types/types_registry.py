6d0ab5232255e2539b1e70476085a34808fba1a3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 14:54:41 -0400

    Update types_registry.py

--- src/vba_types/types_registry.py
+++ src/vba_types/types_registry.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
+from .vba_type_base import VBATypeBase
+from .variable import VBAVariable
 
 
 class VBATypesRegistry:
-    pass
+
+    def coerce(incoming: T | VBATypeBase, declared_type: str) -> VBATypeBase:
+        pass
 
 
 registry = VBATypesRegistry()
