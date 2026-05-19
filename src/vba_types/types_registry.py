cc2f47497d9c83ce8a7968f1f26e57d305073b36
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 15:52:44 -0400

    Add register method to VBATypesRegistry

--- src/vba_types/types_registry.py
+++ src/vba_types/types_registry.py
@@ -11,5 +11,10 @@ class VBATypesRegistry:
     def __init__(self: T) -> None:
         self._registry: dict[tuple[str, type], Callable] = {}
 
+    def register(self: T,
+                 op: str,
+                 incoming_cls: type,
+                 handler: Callable) -> None:
+        self._registry[(op, incoming_cls)] = handler
 
 registry = VBATypesRegistry()
