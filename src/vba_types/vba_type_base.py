7abe550bc1fdce8774ad3205d3f717ebd5c86bd9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-19 22:47:15 -0400

    Update vba_type_base.py

--- src/vba_types/vba_type_base.py
+++ src/vba_types/vba_type_base.py
@@ -38,13 +38,14 @@ class VBATypeBase(ABC):
         return registry.execute("//", self, other)
 
     def __eq__(self: T,                                # type: ignore[override]
-               other: object) -> "VBATypeBase":        # type: ignore[override]
+               other: object) -> "VBATypeBase":
         if not isinstance(other, VBATypeBase):
             return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("==", self, other)
 
-    def __ne__(self: T, other: object) -> "VBATypeBase":
+    def __ne__(self: T,                                # type: ignore[override]
+               other: object) -> "VBATypeBase":
         if not isinstance(other, VBATypeBase):
             return NotImplemented
         from vba_types.vba_registry import registry
