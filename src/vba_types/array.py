256fea550fbba2aafad6d9158acb74af76fb9e8c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-20 07:39:14 -0400

    Change return type to VBATypeBase in array.py
    Refactor return value to use VBATypeBase for consistency.

--- src/vba_types/array.py
+++ src/vba_types/array.py
@@ -19,7 +19,8 @@ class VBAArray(VBATypeBase):
         val = self._data
         for c in coords:
             val = val[c]
-        return val
+        output: VBATypeBase = val
+        return output
 
     def __setitem__(self: T,
                     key: Union[int, Tuple[int, ...]],
