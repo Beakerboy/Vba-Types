ed5dd7dd63391faa3d920045b150cd11eabe2baf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-20 12:09:09 -0400

    Fix import statement for VBAInteger in coercions.py

--- src/vba_types/coercions.py
+++ src/vba_types/coercions.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from .exceptions import TypeMismatchError, InvalidNullError
 from .types_registry import registry
 from .empty import VBAEmpty
-from .integral_types import VBAInteger
+from .integral_type import VBAInteger
 from .null import VBANull
 from .vba_type_base import VBATypeBase
 
