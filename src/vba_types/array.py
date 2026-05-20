f546d318b3853b30e861488bd45e143c018f4b59
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-20 08:56:08 -0400

    Update array.py

--- src/vba_types/array.py
+++ src/vba_types/array.py
@@ -46,7 +46,7 @@ class VBAArray(VBATypeBase):
                    *args: int | tuple[int, int],
                    empty: VBATypeBase) -> T:
         data = list(args)
-        if len(data) == 1 and isinstance(data[0], int):
+        if isinstance(data[0], int):
             input = [empty] * (data[0] + 1)
             return cls(*input)
         else:
