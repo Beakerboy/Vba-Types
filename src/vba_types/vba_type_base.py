e28b41b61caf26c1a0f6ac562a8d17a51bb7cfe2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-20 11:22:23 -0400

    Update vba_type_base.py

--- src/vba_types/vba_type_base.py
+++ src/vba_types/vba_type_base.py
@@ -10,32 +10,44 @@ class VBATypeBase(ABC):
     value: Any
 
     def __add__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("+", self, other)
 
     def __sub__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("-", self, other)
 
     def __mod__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("%", self, other)
 
     def __mul__(self: T, other: T) -> "VBATypeBase":
         if not isinstance(other, VBATypeBase):
-            raise typeError()
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("*", self, other)
 
     def __pow__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("**", self, other)
 
     def __truediv__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("/", self, other)
 
     def __floordiv__(self: T, other: T) -> "VBATypeBase":
+        if not isinstance(other, VBATypeBase):
+            return NotImplemented
         from vba_types.vba_registry import registry
         return registry.execute("//", self, other)
 
