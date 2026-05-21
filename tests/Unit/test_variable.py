526bd875393df65d4079967386d974a1c1244efb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-21 09:12:13 -0400

    Enhance tests for VBAVariable functionality
    Add tests for VBAVariable constructor and variant-string relation

--- tests/Unit/test_variable.py
+++ tests/Unit/test_variable.py
@@ -1,4 +1,6 @@
 from vba_types.variable import VBAVariable
+from vba_types.integer import VBAInteger
+from vba_types.string import VBAString
 from vba_types.empty import Empty
 
 
@@ -6,3 +8,8 @@ def test_constructor() -> None:
     var = VBAVariable()
     assert var.declared_type == "Variant"
     assert var.value is Empty
+
+
+def test_variant_string_relation() -> None:
+    foo = VBAVariable(value=VBAInteger(10))
+    assert foo < VBAString()
