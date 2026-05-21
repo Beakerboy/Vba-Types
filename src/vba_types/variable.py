52fc33d871a1597dce4fa95d14918b36b112c3a4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-21 16:55:15 -0400

    Update variable.py

--- src/vba_types/variable.py
+++ src/vba_types/variable.py
@@ -95,13 +95,13 @@ class VBAVariable:
                     result = self < registry.coerce("integer", other._value)
             else:
                 if str(other._value) == "":
-                    return  VBABoolean(True)
+                    return VBABoolean(True)
                 try:
                     coerce = registry.coerce(self._declared_type, other._value)
                     result = self < coerce
                 except Exception:
                     result = registry.coerce("integer", self._value) < other
-            return  VBABoolean(result)
+            return VBABoolean(result)
         return self._value < self._unwrap(other)
 
     def __ge__(self: T, other: object) -> VBATypeBase:
